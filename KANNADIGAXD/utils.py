894df75c885fbcc2321353220e523320d46350fc
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-26 12:47:44 +0530

    Update utils.py

--- KANNADIGAXD/utils.py
+++ KANNADIGAXD/utils.py
@@ -7,13 +7,13 @@ import inspect
 import re
 
 def load_plugins(plugin_name):
-    path = Path(f"DEADLYSPAM/plugins/{plugin_name}.py")
-    name = "DEADLYSPAM.plugins.{}".format(plugin_name)
+    path = Path(f"KANNADIGAXD/plugins/{plugin_name}.py")
+    name = "KANNADIGAXD.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["DEADLYSPAM.plugins." + plugin_name] = load
+    sys.modules["KANNADIGAXD.plugins." + plugin_name] = load
     print("[INFO] Successfully Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
